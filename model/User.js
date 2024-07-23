3ebc5d6ff91b6b53136fa767ec6ecf7aefe7e27b
Sai Methish <Sai.Methish@godigit.com>
2024-07-23 17:29:30 +0530

    added model

---
+++ model/User.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(fullName,addrress,age,number,state){
+        this.fullName=fullName;
+        this.addrress=addrress;
+        this.age=age;
+        this.number=number;
+        this.state=state;
+    }
+};
