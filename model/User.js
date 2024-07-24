b2415905296f3190f4cde285024f09b9baed7581
Sai Methish <Sai.Methish@godigit.com>
2024-07-24 11:21:25 +0530

    c

--- model/User.js
+++ model/User.js
@@ -1,50 +1,52 @@
-class User {
-    fullName="";
-    addrress="";
-    age=0;
-    number="";
-    state="";
+export default class User {
+    constructor() {
+        this.fullName = "";
+        this.addrress = "";
+        this.age = 0;
+        this.number = "";
+        this.state = "";
+    }
     //read
     getFullName() {
         return this.fullName;
     }
     //create and update
     setFullName(fullName) {
-        this.fullName=fullName;
+        this.fullName = fullName;
     }
     //delete 
-    deleteFullName(){
-        this.fullName="";
+    deleteFullName() {
+        this.fullName = "";
     }
     //...........
 
-    getAddress(){
+    getAddress() {
         return this.addrress;
     }
-    setAddress(address){
-        this.addrress=address;
+    setAddress(address) {
+        this.addrress = address;
     }
-    deleteAddress(){
-        this.addrress="";
+    deleteAddress() {
+        this.addrress = "";
     }
     //.............
-    getAge(){ return this.age;}
-    setAge(age){ this.age=age;}
-    deleteAge(){ this.age=0;}
+    getAge() { return this.age; }
+    setAge(age) { this.age = age; }
+    deleteAge() { this.age = 0; }
 
     //.......
-    getNumber(){ return this.number;}
-    setNumber(number){ this.number=number;}
-    deleteNumber(){this.number="";}
+    getNumber() { return this.number; }
+    setNumber(number) { this.number = number; }
+    deleteNumber() { this.number = ""; }
     //......
 
-    getState(){ return this.state;}
-    setState(state){ this.state=state;}
-    deleteState(){ this.state="";}
+    getState() { return this.state; }
+    setState(state) { this.state = state; }
+    deleteState() { this.state = ""; }
     //.....
-    printDetails(){
+    printDetails() {
         return `fullname= ${this.fullName} address= ${this.addrress} age= ${this.age}
         number= ${this.number} state=${this.state}`;
     }
 };
-export default User;
+
