81959ccb549fc2c33894432d61612bacd0c9940f
Sai Methish <Sai.Methish@godigit.com>
2024-07-25 13:07:15 +0530

    addingsearch

--- service/addressBookService.js
+++ service/addressBookService.js
@@ -2,6 +2,8 @@ import User from "../model/User.js";
 import PromptSync  from "prompt-sync";
 const prompt=PromptSync();
 
+const SIZE=prompt("enter size ");
+const userArr=[]
 const entryDetails = (u) => {
     u.fullName = prompt("Enter fullName");
     u.addrress = prompt("Enter address");
@@ -10,10 +12,62 @@ const entryDetails = (u) => {
     u.state = prompt("Enter state");
 };
 
-let u1=new User();
-entryDetails(u1);
-console.log(u1.printDetails());
+for(let i=0;i<SIZE;i++){
+    let u=new User();
+    entryDetails(u);
+    userArr.push(u);
+}
+console.log(userArr);
 
+//.........
+
+const searchUser=(param,val)=>{
+    for(let i=0;i<userArr.length;i++){
+        if(param==1 && userArr[i].fullName==val) return userArr[i];
+        if(param==2 && userArr[i].address==val) return userArr[i];
+        if(param==3 && userArr[i].age==val) return userArr[i];
+        if(param==4 &&  userArr[i].number==val) return userArr[i];
+        if(param==5 && userArr[i].state==val) return userArr[i];
+    }
+    return `${param} not found`;
+}
+
+
+console.log(searchUser(3,"22"));
+
+//
+
+const sortUser=(param)=>{
+    let res;
+    if(param=="fullName"){
+        userArr.sort((a,b)=>{
+            return a.fullName<b.fullName;
+        });
+    }
+    if(param=="address"){
+        userArr.sort((a,b)=>{
+            return a.address<b.address;
+        });
+    }
+    if(param=="age"){
+        userArr.sort((a,b)=>{
+            return  parent(a.age)<parseInt(b.age);
+        });
+    }
+    if(param=="number"){
+        userArr.sort((a,b)=>{
+            return a.number<b.number;
+        });
+    }
+    if(param=="state"){
+        userArr.sort((a,b)=>{
+            return a.state<b.state;
+        });
+    }
+}
+
+sortUser("age");
+console.log(userArr);
 
 
 
